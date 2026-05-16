02d37660e28e2113a9915204275867e9338e1de9
Yann Hallage <contact.devhllg@gmail.com>
2026-05-16 12:28:56 +0000

    djdjjdd

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,7 @@ export { createApp } from "./create-app";
 const app = createApp();
 
 export default app;
+
+module.exports = app;
+module.exports.default = app;
+module.exports.createApp = createApp;
